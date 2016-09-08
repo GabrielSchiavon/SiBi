8b7e6ae2cc2d5cadfef7a950192ba4312845114e
schiavon <schiavon@schiavon>
2016-09-08 08:23:51 -0300

    Classe pessoa adicionada

---
+++ src/modelo/Pessoa.java
@@ -0,0 +1,195 @@
+/*
+ * Trabalhodesenvolvido para disciplina de ISS - 2016
+ */
+package modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author schiavon
+ */
+public class Pessoa {
+    private int id;
+    private String nome;
+    private int cpf;
+    private int rg;
+    private Date dataNasc;
+    private int cep;
+    private String endereço;
+    private String bairro;
+    private int numero;
+    private String complemento;
+    private String email;
+    private String senha;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public int getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the dataNasc
+     */
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    /**
+     * @param dataNasc the dataNasc to set
+     */
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    /**
+     * @return the cep
+     */
+    public int getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the endereço
+     */
+    public String getEndereço() {
+        return endereço;
+    }
+
+    /**
+     * @param endereço the endereço to set
+     */
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
