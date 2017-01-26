ddec78f8602d035c44530a1f4343733d8e0fe7ce
Gabriel <Gabriel@Gabriel-PC>
2017-01-26 00:18:11 -0200

    Alguns ajustes

--- src/modelo/Reserva.java
+++ src/modelo/Reserva.java
@@ -3,15 +3,72 @@
  */
 package modelo;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.*;
 
 /**
  *
  * @author schiavon
  */
-class Reserva {
+@Entity
+@Table(name = "reserva")
+class Reserva implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int id;
+    @Column(nullable = false)
+    private int idMaterial;
+    @Column(nullable = false)
+    private int idUsuario;
+    @Column(nullable = false)
+    private int proxReserva;
+    @Temporal(TemporalType.DATE)
     private Date dataReserva;
 
+    public Reserva(){
+        this.proxReserva = -1;
+    }
+    
+    public Reserva(int idMaterial, int idUsuario, Date dataReserva) {
+        this.idMaterial = idMaterial;
+        this.idUsuario = idUsuario;
+        this.dataReserva = dataReserva;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdMaterial() {
+        return idMaterial;
+    }
+
+    public void setIdMaterial(int idMaterial) {
+        this.idMaterial = idMaterial;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getProxReserva() {
+        return proxReserva;
+    }
+
+    public void setProxReserva(int proxReserva) {
+        this.proxReserva = proxReserva;
+    }
+    
     public Date getDataReserva() {
         return dataReserva;
     }
